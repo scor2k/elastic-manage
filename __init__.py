c4b143c80b40c5506dfb0a9fa8755d2fb61bf5bc
Alexander K <scor2k@gmail.com>
2020-04-01 11:44:50 +0300

    SA-7258 remove old version and update req.txt

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-__version__ = "0.3.8"
-__log_type__ = "sa-admin-elastic"
-
-__application__ = "Elasticsearch Management System"
-
-# use for store settings
-__app_name__ = "elastic-manage"
--- sa_tools/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-from .logs      import saLogs
-from .elastic   import saElastic
-from .config    import saConfig
